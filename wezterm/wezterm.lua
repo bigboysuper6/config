e22d57ca125ab54ad1e51a25a1bd2b5b6b5f4ae6
Jaron <jaron@Jarons-MacbookPro.local>
2025-06-03 00:05:37 +0800

    update wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -18,8 +18,6 @@ config = {
     window_background_opacity = 0.3,
     macos_window_background_blur = 20,
 	default_cursor_style = 'BlinkingBar',
-    initial_cols = 120,
-    initial_rows = 40,
     window_decorations = "NONE",
 }
 
@@ -36,8 +34,35 @@ config.background = {{
         brightness = 0.25
     },
     opacity = 0.7,
-    s
 }}
+
+local function center_window_once(window)
+  wezterm.GLOBAL.windows_centered = wezterm.GLOBAL.windows_centered or {}
+
+  local window_id = window:window_id() .. ""
+  if wezterm.GLOBAL.windows_centered[window_id] then
+    return
+  end
+
+  local screen = wezterm.gui.screens().active
+  local ratio = 0.6
+  local width = screen.width * ratio
+  local height = screen.height * ratio
+
+  window:set_inner_size(width, height)
+
+  local dimensions = window:get_dimensions()
+  local x = (screen.width - dimensions.pixel_width) * 0.5
+  local y = (screen.height - dimensions.pixel_height) * 0.5
+
+  wezterm.GLOBAL.windows_centered[window_id] = true
+
+  window:set_position(x, y)
+end
+
+wezterm.on("update-status", function(window)
+  center_window_once(window)
+end)
 
 -- Finally, return the configuration to wezterm:
 return config
