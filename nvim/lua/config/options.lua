c46a56ac05144ac6c87cae53be0e0df3c334caca
bigsur <bigsur@bigsurdeMacBook-Pro.local>
2025-06-14 17:57:59 +0800

    update

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -5,3 +5,4 @@
 -- Enable the option to require a Prettier config file
 -- If no prettier config file is found, the formatter will not be used
 vim.g.lazyvim_prettier_needs_config = true
+vim.g.lazyvim_picker = "telescope"
